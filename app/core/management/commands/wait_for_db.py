e7c3e7a985dccb44fd23e100a9c0ab5cc7336816
Vikas Kumar <vkskmr858@gmail.com>
2022-11-16 18:51:31 +0530

    Fix for mock errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,7 @@
 """
 Django command to wait for the database to be available.
 """
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
 
 class  Command(BaseCommand):
     """Django command to wait for database."""

3177b6250dd5648c6f3664a86bdcb69dc3f5d50c
Vikas Kumar <vkskmr858@gmail.com>
2022-11-18 23:45:21 +0530

    Fix for users

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -19,7 +19,7 @@ class ModelTests(TestCase):
             password=password,
         )
 
-        self.assertEqual(user.email,email),
+        self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
@@ -31,7 +31,7 @@ class ModelTests(TestCase):
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
-            users = get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):

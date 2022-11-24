c35e827c51d67b707cadcc5eaed506c2a94aa8f4
Vikas Kumar <vkskmr858@gmail.com>
2022-11-25 00:15:25 +0530

    Url mapped for user api

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+
     'rest_framework',
     'drf_spectacular',
+    'core',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -134,5 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULF_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -14,12 +14,12 @@ class AdminSiteTests(TestCase):
         """Create user and client."""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='user@example.com',
+            email='admin@example.com',
             password='testpass123',
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='admin@example.com',
+            email='user@example.com',
             password='testpass123',
             name='Test User'
         )
